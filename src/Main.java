6f38b681693f54e646acfcfefb928607a637dcd1
Iliyan Kostov <iliyan.kostov@email.ikostov.org>
2023-11-08 21:16:57 +0200

    Created new java class for database

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,10 @@
-import java.io.*;
-import java.sql.*;
-
 public class Main {
     public static void main(String[] arg) {
-        String SQL = "SELECT datname FROM pg_database;";
+        String sqlGetDatabases = "SELECT datname FROM pg_database;";
 
-        try (Connection conn = DatabaseConnection.connectToRDS();
+        ChatDBManager chatDBManager = ChatDBManager.getInstance();
 
-             PreparedStatement pst = conn.prepareStatement(SQL);
-             ResultSet rs = pst.executeQuery()) {
-            while (rs.next()) {
-                //int id = rs.getInt("id");
-                String datname = rs.getString("datname");
-                System.out.println(datname);
-            }
-        } catch ( SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
+        String[] queryResult = chatDBManager.executeQuery(sqlGetDatabases, "datname");
 
-   }
+    }
 }
