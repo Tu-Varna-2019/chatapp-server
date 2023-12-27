5f03fe82902373ed3715e859f39545b4debe0b68
Iliyan Kostov <iliyan.kostov@email.ikostov.org>
2023-12-27 11:04:20 +0200

    Add SendFriendRequestEventHandler and update User model

--- src/controller/events/EventHandlerRegistry.java
+++ src/controller/events/EventHandlerRegistry.java
@@ -3,17 +3,7 @@ package controller.events;
 import java.util.HashMap;
 import java.util.Map;
 
-import controller.events.handlers.ChangePasswordEventHandler;
-import controller.events.handlers.DeleteAccountEventHandler;
-import controller.events.handlers.DeleteMessageByGroupIDEventHandler;
-import controller.events.handlers.GetFriendRequestsAuthUserEventHandler;
-import controller.events.handlers.GetGroupChatsAuthUserEventHandler;
-import controller.events.handlers.GetMessagesByGroupIDEventHandler;
-import controller.events.handlers.LoginEventHandler;
-import controller.events.handlers.RenameEmailEventHandler;
-import controller.events.handlers.RenameUsernameEventHandler;
-import controller.events.handlers.SendMessageByGroupIDEventHandler;
-import controller.events.handlers.SignUpEventHandler;
+import controller.events.handlers.*;
 
 /*
  * This class is used to register all the event handlers
@@ -27,6 +17,7 @@ public class EventHandlerRegistry {
         eventHandlerMap.put("Login", new LoginEventHandler());
         eventHandlerMap.put("GetGroupChatsAuthUser", new GetGroupChatsAuthUserEventHandler());
         eventHandlerMap.put("GetFriendRequestsAuthUser", new GetFriendRequestsAuthUserEventHandler());
+        eventHandlerMap.put("SendFriendRequest", new SendFriendRequestEventHandler());
         eventHandlerMap.put("DeleteAccount", new DeleteAccountEventHandler());
         eventHandlerMap.put("RenameUsername", new RenameUsernameEventHandler());
         eventHandlerMap.put("RenameEmail", new RenameEmailEventHandler());
